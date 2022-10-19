44406f074e06a5e401b329e97efcd4db206be736
Jakub Jóźwiak <jozwiak.ja@icloud.com>
2022-10-19 08:18:44 +0200

    I created components needed to build post details page, created [slug].js file, to change routes between pages

---
+++ components/Comments.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Comments = () => {
+  return <div>PostDetail</div>;
+};
+
+export default Comments;
---
+++ components/CommentsForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommentsFrom = () => {
+  return <div>CommentsForm</div>;
+};
+
+export default CommentsFrom;
